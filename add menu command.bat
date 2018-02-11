b90d1008703a4f699b8dca1476be2ad372572bfb
wil <schockwil@gmail.com>
2018-02-10 17:55:11 -0700

    initial

---
+++ add menu command.bat
@@ -0,0 +1,13 @@
+set commandName="Find Handles"
+set handlePath="path to handle here"
+
+::add command for files
+reg add "HKCR\*\shell\FindHandle" /ve /d %commandName%
+reg add "HKCR\*\shell\FindHandle\command" 
+reg add "HKCR\*\shell\FindHandle\command" /ve /d "cmd.exe /K %handlePath% \"%%1\"" /f
+
+::add command for folders
+reg add "HKCR\Directory\shell\FindHandle" /ve /d %commandName%
+reg add "HKCR\Directory\shell\FindHandle\command"
+reg add "HKCR\Directory\shell\FindHandle\command" /ve /d "cmd.exe /K %handlePath% \"%%1\"" /f
+pause
