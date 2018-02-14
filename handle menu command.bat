ad8e89087b71f47364990e66b34b89d7a0410a56
wil <schockwil@gmail.com>
2018-02-13 18:18:58 -0700

    remove redundant command

--- handle menu command.bat
+++ handle menu command.bat
@@ -2,12 +2,10 @@ set commandName="Find Handles"
 set handlePath="path to handle here"
 
 ::add command for files
-reg add "HKCR\*\shell\FindHandle" /ve /d %commandName%
-reg add "HKCR\*\shell\FindHandle\command" 
+reg add "HKCR\*\shell\FindHandle" /ve /d %commandName% /f
 reg add "HKCR\*\shell\FindHandle\command" /ve /d "cmd.exe /K %handlePath% \"%%1\"" /f
 
 ::add command for folders
-reg add "HKCR\Directory\shell\FindHandle" /ve /d %commandName%
-reg add "HKCR\Directory\shell\FindHandle\command"
+reg add "HKCR\Directory\shell\FindHandle" /ve /d %commandName% /f
 reg add "HKCR\Directory\shell\FindHandle\command" /ve /d "cmd.exe /K %handlePath% \"%%1\"" /f
 pause
